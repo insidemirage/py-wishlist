0cb9bb87781a5893db385e9ec036c3669699ee71
insidemirage <vladislavlightinthedarkness@gmail.com>
2020-02-07 23:51:38 +0300

    useless files removed

--- tex.py
+++
@@ -1,4 +0,0 @@
-import pymysql
-import config
-pymysql.connect(config.host, config.user, config.password, config.database)
-# docker run -d --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=secret -e MYSQL_DATABASE=mySchema mysql
